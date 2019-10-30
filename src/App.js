f2ecde82acdfb3e07fab83e4a870eb7e37cd8d06
donkim <dickings@likelion.org>
2019-10-30 10:53:06 +0900

    submit 이벤트 발생시 데이터 배열에 저장하기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    information: []
+  }
+
+  handleCreate = (data) => {
+    const {information} = this.state
+    this.setState({
+      information: information.concat(data)
+    })
+  }
+
+  render() {
+    return (
+      <fragment>
+        <div>
+          <PhoneForm onCreate={this.handleCreate}/>
+          <div>
+            목록 : <br/>
+            {JSON.stringify(this.state.information)}
+          </div>
+        </div>
+      </fragment>
+    );
+  }
 }
 
 export default App;
